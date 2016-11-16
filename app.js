b7ba1b58689c910e8747936a3713544c42b2e922
alexandre <alexandre@DESKTOP-MCLOQ4L>
2016-11-16 14:59:02 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var Twig = require("twig");
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var index = require('./lib/router/index');
+var app = express();
+
+console.log("toto");
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'twig');
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/', index);
+//app.use('/users', users);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
